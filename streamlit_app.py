5661f1b79dfb07169c92c1044c48ea330115286d
rbillimoria987 <127286846+rbillimoria987@users.noreply.github.com>
2023-03-18 14:30:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit;
 import pandas;
 import requests
 
+streamlit.header("Fruityvice Fruit Advice!")
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
